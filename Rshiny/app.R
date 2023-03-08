c49f440b7aa0a087c772565c34c284e8fc6c524a
lzy66666666 <leelzy236@gmail.com>
2023-03-09 01:06:31 +0800

    1st draft of correlation plot

--- Rshiny/app.R
+++ Rshiny/app.R
@@ -1,5 +1,7 @@
 library(shiny)
+library(corrplot)
 library(tidyverse)
+
 
 # Datset loading
 newloan <- read_csv("data/loan_data_v2/new_loans_cleaned.csv")
@@ -25,44 +27,48 @@ ui <- fluidPage(
   
   # Application title
   titlePanel("Loan Default Prediction"),
-  # Input selections on the left panel
 
-  # New or repeat loan
-  sidebarPanel(
-    selectInput(inputId = "LoanType",
-                label = "Select type of loans",
-                choices = c("Single loan" = "SINGLE LOAN",
-                            "Repeat loans" = "REPEAT LOANS"),
-                selected = "SINGLE LOAN"),
-  # Allow selection of variables from the chosen loan's dataset    
-    uiOutput(outputId = "Variables"),
-    uiOutput(outputId = "Correlations"),
-
-    sliderInput(inputId = "yearSlider",
-                label = "Year",
-                min = 2017,
-                max = 2019,
-                value = 2017,
-                step = 1,
-                sep = "")),
   # Tabset panel with three options at the top of the main panel
   tabsetPanel(
     tabPanel("Explore",
              tabsetPanel(
-               tabPanel("Bivariate Analysis"),
-               tabPanel("Correlation Analysis")
+               tabPanel("Bivariate Analysis",
+                        # Input selections on the left panel
+                        
+                        # New or repeat loan
+                        sidebarPanel(
+                          selectInput(inputId = "LoanType",
+                                      label = "Select type of loans",
+                                      choices = c("Single loan" = "SINGLE LOAN",
+                                                  "Repeat loans" = "REPEAT LOANS"),
+                                      selected = "SINGLE LOAN"),
+                          # Allow selection of variables from the chosen loan's dataset    
+                          uiOutput(outputId = "Bi_Variable_Y"),
+                          uiOutput(outputId = "Bi_Variable_X")),
+                        mainPanel(plotOutput(outputId = "barchart1"))
+                        ),
+               tabPanel("Correlation Analysis",
+                        # Input selections on the left panel
+                        
+                        # New or repeat loan
+                        sidebarPanel(
+                          selectInput(inputId = "LoanType",
+                                      label = "Select type of loans",
+                                      choices = c("Single loan" = "SINGLE LOAN",
+                                                  "Repeat loans" = "REPEAT LOANS"),
+                                      selected = "SINGLE LOAN"),
+                          # Allow selection of variables from the chosen loan's dataset    
+                          uiOutput(outputId = "Corr_Variables")),
+                        mainPanel(plotOutput(outputId = "Correlationplot"))
+                        
+                        )
              )),
-    tabPanel("Analysis"),
-    tabPanel("Predict")
-  ),
-  # Mainpanel 
-  mainPanel(
-    # Upper plot
-    plotOutput(outputId = "correlationplot"),
-    
-    # Lower plot
-    plotOutput(outputId = "histogram")
-  ))
+    tabPanel("Analysis",
+             mainPanel("Testing")),
+    tabPanel("Predict",
+             mainPanel("Testing2"))
+  )
+)
 
 # Define server logic required to allow reading of different dataset
 server <- function(input, output) {
@@ -74,32 +80,59 @@ server <- function(input, output) {
     }
   })
   
-  output$Variables <- renderUI({
-    selectInput(inputId = "Variable",
-                label = "Select variables from below for distribution analysis",
+  output$Corr_Variables <- renderUI({
+    selectInput(inputId = "Variables",
+                label = "Select variables from below",
+                choices = c("approval_duration",
+                            "loanamount",
+                            "totaldue",
+                            "age_at_loan",
+                            "longitude_gps",
+                            "latitude_gps"),
+                multiple = TRUE)
+  })
+  output$Bi_Variable_Y <- renderUI({
+    selectInput(inputId = "Bi_Variable_Y",
+                label = "Select variable Y from below",
                 choices = names(data()),
                 multiple = TRUE)
   })
-  output$Correlations <- renderUI({
-    selectInput(inputId = "Correlations",
-                label = "Select the pair of variables for correlation analysis",
+  output$Bi_Variable_X <- renderUI({
+    selectInput(inputId = "Bi_Variable_X",
+                label = "Select variable X from below",
                 choices = names(data()),
                 multiple = TRUE)
   })
+  # output$Variables <- renderUI({
+  #   selectInput(inputId = "Variables",
+  #               label = "Select variables from below",
+  #               choices = names(data()),
+  #               multiple = TRUE)
+  # })
   
   
   
-  
-  # [CORRECTION NEEDED. NOT VERY SURE HOW TO HANDLE THIS PART]
-  # Define server logic required to draw histogram and correlation plot
-  output$correlationplot <- renderPlot({
-    # plot code to be input
+# Correlation plot  
+  output$Correlationplot <- renderPlot({
+    # To check if inputs are available before generation of plots
+    req(input$LoanType)
+    req(input$Variables)
+    var_data <- data()[, input$Variables]
+    corr_data <- cor(var_data)
+    # Using corrplot from corrplot library
+    corrplot(corr_data,
+             method = "ellipse",
+             diag = FALSE,
+             type = "lower")
+
   })
+  
+# Below is not updated yet
   output$histogram <- renderPlot({
     if(!is.null(input$Variable)) {
       hist_data_numeric <- as.numeric(data()[input$Variable]) 
       ggplot(hist_data_numeric,
-           aes(x = .data)) +
+             aes(x = .data)) +
         geom_histogram() +
         labs(title = "Distribution of Selected Variables")}
   })
