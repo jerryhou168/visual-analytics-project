57cb356fd2a0c0ad379892da03c9f27dc89e341a
thomashoanghuy <thomashoanghuy17@gmail.com>
2023-03-17 00:17:28 +0800

    thomas 17/3/23 commit

--- Rshiny/univariate.R
+++ Rshiny/univariate.R
@@ -6,13 +6,12 @@ t1_loanType <- selectInput(inputId = "t1_loanType",
                            selected = "S")
 
 # Define the variables to be studied in this tab
+
 newloan_options <- setNames(newloan_factors, 
                             newloan_factors)
 
 repeatloan_options <- setNames(repeatloan_factors, 
                                repeatloan_factors)
-
-
 
 t1_variable_x <- uiOutput(outputId = "Uni_Variable_X")
 
@@ -27,7 +26,7 @@ univariate_nav <- fluidRow(
 
 
 univariate_main <-fluidRow(
-  plotOutput(outputId = "Univarplot",
+  plotOutput("Univarplot",
              width = "100%",
              height = 400)
   
@@ -36,48 +35,70 @@ univariate_main <-fluidRow(
 
 # Server
 
-univar <- function(input, output, session) {
+univar <- function(input, output) {
 
-  
-  univar_opt <- reactive({
-      if (input$t1_loanType == "S") {
-        return(newloan_options)
-      } else {
-        return(repeatloan_options)
-      }
-    })
-  
-  output$Uni_Variable_X <- renderUI({
-      selectInput(inputId = "Uni_Variable_X",
-                  label = "Select variable X from below",
-                  choices = names(univar_opt()),
-                  multiple = FALSE)
-    })
-
-  
-  univar_data <- reactive({
-      if (input$t1_loanType == "S") {
-        return(newloan)
-      } 
-    else {
-         return(repeatloan)
-       }
+univar_opt <- reactive({
+    if (input$t2_loanType == "S") {
+      return(newloan_options)
+    } else {
+      return(repeatloan_options)
+    }
   })  
 
-  output$Univarplot <- renderPlot({
-    # Barchart for 1 variable   
-    {
-    p1 <- ggplot(univar_data(), aes(x =input$Uni_Variable_X)) +
-        geom_histogram()
-      
-      p1
+input$Uni_Variable_X <- renderUI({
+    selectInput(outputId = "Uni_Variable_X",
+                label = "Select variable X from below",
+                choices = names(univar_opt()),
+                multiple = FALSE)
+  })
+  
+  
+
+univar_data <- reactive({
+    if (input$t1_loanType == "S") {
+      return(newloan)
+    } else {
+      return(repeatloan)
     }
-    
-    })
+  })
+
+output$Univarplot <- renderPlot({
+  
+  if (input$Uni_Variable_X %in% c("bank_name_clients",
+                                 "approval_duration_group",
+                                 "age_at_loan_25th_pctile",
+                                 "credit_rating",
+                                 "termdays",
+                                 "employment_status_risk",
+                                 "level_of_education_risk",
+                                 "referral",
+                                 "bank_account_type_recode",
+                                 "loanamount_group",
+                                 "loanamount",
+                                 "Interestrate",
+                                 "bank_account_type",
+                                 "employment_status",
+                                 "total_referrrals",
+                                 "max_interest_rate",
+                                 "mean_interest_rate",
+                                 "mean_referrals",
+                                 "max_churn_flag",
+                                 "loanamount"))
+  {  
+  p <- ggplot(univar_data(), aes(x = data[[input$Uni_Variable_X]])) +
+      geom_histogram()
+  
+  p
   }
+
+
+})
+
+}
         
 
   
+
   
   
   
